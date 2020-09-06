ab09b0d5e5f2b4e75ee055eb887c957b556eea6c
Zer024 <70179452+Zer024@users.noreply.github.com>
2020-09-06 19:07:37 -0400

    Chapter 2 Page 7 exercise 1
    Imported csv file.

---
+++ Chapter2page7.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[9]:
+
+
+import pandas as pd
+Location = "/Users/asc62/Downloads/all_040_in_12.P1.csv"
+df = pd.read_csv(Location)
+print(df)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
